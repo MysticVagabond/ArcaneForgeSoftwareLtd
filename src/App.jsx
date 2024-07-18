958509026d37d280eeda9e6c5dbe81e282ed03de
MysticVagabond <63424123+MysticVagabond@users.noreply.github.com>
2024-07-18 20:36:18 +0100

    Adding Solid JS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>Arcane Forge Software</h1>
+      <p>A humble beginning. </p>
+      <p>Now with Solid JS</p>
+    </>
+  );
+}
+
+export default App;
